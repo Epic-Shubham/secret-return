138034d6f37cd544290da156190ef8840b1bad57
Epic-Shubham <163536954+Epic-Shubham@users.noreply.github.com>
2024-03-17 16:43:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnura /AutoAnura
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoAnura
 fi
 cd /AutoAnura
 pip3 install -U -r requirements.txt
